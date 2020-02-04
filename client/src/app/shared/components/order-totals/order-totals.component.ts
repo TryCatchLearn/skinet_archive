15ed63b793aca496c43b3c79041b0c35862184a3
trycatchlearn <trycatchlearn@outlook.com>
2020-02-04 11:13:47 +0700

    End of section 20

--- client/src/app/shared/components/order-totals/order-totals.component.ts
+++ client/src/app/shared/components/order-totals/order-totals.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IBasketTotals } from '../../models/basket';
 import { BasketService } from 'src/app/basket/basket.service';
@@ -9,12 +9,13 @@ import { BasketService } from 'src/app/basket/basket.service';
   styleUrls: ['./order-totals.component.scss']
 })
 export class OrderTotalsComponent implements OnInit {
-  basketTotal$: Observable<IBasketTotals>;
+  @Input() shippingPrice: number;
+  @Input() subtotal: number;
+  @Input() total: number;
 
-  constructor(private basketService: BasketService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.basketTotal$ = this.basketService.basketTotal$;
   }
 
 }
